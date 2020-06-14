55b252ff477b2835b46b49cc0e27ab858c803395
Arpit Rana <arpitran@buffalo.edu>
2020-06-13 22:33:27 -0400

    Update solution.py

---
+++ Python/Set .union() Operations/solution.py
@@ -0,0 +1,6 @@
+n =int(input("No. of students subscrbed to English newspapers")) # No. of students subscribed to English newspapers
+N = set(map(int,input("# Space separated roll numbers of students in 'n'").split())) # Space separated roll numbers of students in 'n'
+b = int(input("# Number of students described to French newspapers")) # Number of students described to French newspapers
+B = set(map(int,input("# Space separated roll numbers of students in 'b'").split())) # Space separated roll numbers of students in 'b'
+
+print(len(N.union(B)))
