be2083bd7d798b89b26283685396f91f714842df
Arpit Rana <arpitran@buffalo.edu>
2020-06-14 17:55:04 -0400

    Find the Runner-Up Score!

---
+++ Python/Find the Runner-Up Score!/solution.py
@@ -0,0 +1,4 @@
+if __name__ ==' __main__':
+    n = int(input("Enter number of scores"))
+    arr =list(set(map(int,input("Enter space separated list of scores").split())))
+    print(sorted(arr,reverse=True)[1])
