11ed621eed426e604a8a6b549f4c4271b16db7db
Arpit Rana <arpitran@buffalo.edu>
2020-06-16 23:33:03 -0400

    Floor, Ceil and Rint solution added

---
+++ Python/Floor, Ceil and Rint/solution.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import numpy
+
+numpy.set_printoptions(sign=' ')
+
+S = numpy.array(list(map(float,input("Enter space separatef float integers").split())))
+
+print(numpy.floor(S))
+print(numpy.floor(S))
+print(numpy.rint(S))
