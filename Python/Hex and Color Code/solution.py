c364e835edfe7bb78121c82f016ed8d3259a1578
Arpit Rana <arpitran@buffalo.edu>
2020-06-21 12:49:50 -0400

    Mod and Divmod

---
+++ Python/Hex and Color Code/solution.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import re # Regular expression package in Python
+
+T =int(input())
+
+in_css = False
+for _ in range(T):
+    s = input()
+    if '{' in s:
+        in_css = True
+    elif '}' in s:
+        in_css = False
+    elif in_css:
+        for color in re.findall('#[0-9a-fA-F]{3,6}',s):
+            print(color)
---
+++ Python/Mod Divmod/solution.py
@@ -0,0 +1,7 @@
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+
+res = divmod(a,b)
+print(res[0])
+print(res[1])
+print(res)
