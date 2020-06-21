168592c87f54b0b31895493afa0f545902659b1a
Arpit Rana <arpitran@buffalo.edu>
2020-06-21 13:04:07 -0400

    Triangle Quest

---
+++ Python/Power - Mod Power/solution.py
@@ -0,0 +1,6 @@
+a = int(input("enter first number "))
+b = int(input("enter second number "))
+m = int(input("enter the third number "))
+
+print(pow(a,b))
+print(pow(a,b,m))
---
+++ Python/Triangle Quest/solution.py
@@ -0,0 +1,2 @@
+for i in range(1,int(input("Enter a number between 1 - 9 "))):
+    print((10**(i)//9)*i)
